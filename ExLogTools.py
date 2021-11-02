e18c8d61f235090ed8e41bb3d39fb05825525703
jeremywrk <devnull@localhost>
2021-11-02 10:22:20 -0400

    Add json for saving where log path is change start/stop capture into a single button toggle

--- ExLogTools.py
+++ ExLogTools.py
@@ -1,11 +1,13 @@
 #
+# Exiles Log Tools
 #
-#
+# Stuff for processing the conan log file in real time.
 
 import PySimpleGUI as sg
 import ctypes
 import platform
 from main import ExiLogger
+from jparse import jconf
 
 def make_dpi_aware():
 	# Fix blurry text on high dpi monitors
@@ -15,12 +17,16 @@ def make_dpi_aware():
         ctypes.windll.shcore.SetProcessDpiAwareness(True)
         
 OutVals = ""
-logThread = ""
+#logThread = ""
 Logging = 0
+capture = False
+
+config = jconf()
+LogPath = config.config("LogPath")
         
 Layout = [
-		[sg.Text("Log File:"),sg.Input(size=(80,0),key="LogPath"),sg.FileBrowse(file_types=(("Log Files","*.log"),))],
-		[sg.Button("Start Capture",key="CaptureStart"),sg.Button("Stop Capture",key="CaptureStop"),sg.Text("  "),sg.Button("Clear Counters",key="CounterClear"),sg.Button("Clear Window",key="WindowClear")],
+		[sg.Text("Log File:"),sg.Input(LogPath,size=(80,0),key="LogPath"),sg.FileBrowse(file_types=(("Log Files","*.log"),))],
+		[sg.Button("Start Capture",key="CaptureToggle"),sg.Text("  "),sg.Button("Reset Counters",key="CounterClear"),sg.Button("Clear Window",key="WindowClear")],
 		[sg.Text("Combat Log:")],
 		[sg.Listbox(values=(OutVals),size=(100,20),enable_events=True,key="LogOutput")]
 	]
@@ -42,21 +48,19 @@ while True:
 		# Clear log in window
 		Logging.ClearWindow(window)
 		
-	elif event == "CaptureStop":
-		# Stop capturing logs
-		#capture_stop(logThread,xEvent)
-		Logging.stop()
-		
-	elif event == "CaptureStart":
-		# Start capturing logs
-		#print("Values: ")
-		#print(repr(values))
-		
-		Logging = ExiLogger()
-		Logging.start(window,values['LogPath'],True)
-		#print("LogPath  : {0}".format(values['LogPath']))
-		#logThread, xEvent = capture_start(window,values['LogPath'],True)
-		#toggle_xEvent("start",xEvent)
+	elif event == "CaptureToggle":
+		# Toggle between starting/stopping capture of log
+		if capture == False:
+			# Start
+			Logging = ExiLogger()
+			Logging.start(window,values['LogPath'],True)
+			# Rename button to stop			
+			window['CaptureToggle'].update("Stop Capture")			
+			capture = True
+		elif capture == True:
+			Logging.stop()
+			window['CaptureToggle'].update("Start Capture")		
+			capture = False
 		
 	elif event == sg.WIN_CLOSED:
 		break
--- main.py
+++ main.py
@@ -1,78 +1,79 @@
 #
-# Core functions
+# Core functions, as a class
 #
 import threading
 import os, time
 import PySimpleGUI as sg
 
-xEvent = 0
-
-def toggle_xEvent(value,event):
-
-	if value == "start":
-		xEvent = event
-	elif value == "stop":
-		capture_stop(x,xEvent)
-
-def follow(thefile,keepalive):
-	# Constantly check logfile for changes
-	thefile.seek(0, os.SEEK_END)
+class ExiLogger:
+	
+	def follow(self,thefile,keepalive):
+		# The actual monitoring of the log file
 		
-	while keepalive:
-		if xEvent.is_set():
-			keepalive = False
-		line = thefile.readline()
-		if not line:
-			time.sleep(0.1)
-			continue
-		yield line
+		# Constantly check logfile for changes
+		thefile.seek(0, os.SEEK_END)
+			
+		while keepalive:
+			if self.xEvent.is_set():
+				keepalive = False
+				print("Keep Alive: FALSE")
+			line = thefile.readline()
+			if not line:
+				time.sleep(0.1)
+				continue
+			yield line
+			
+	def start(self,window,logpath,keepalive):
+		# Start capturing
+		self.xEvent = threading.Event()
+		self.x = threading.Thread(target=self.capture, args=(window,logpath,True,))
+		self.x.start()
+		
+		return self.x, self.xEvent
+		
+	def stop(self):
+		# Stop Capturing
+		print("Setting event")
+		self.xEvent.set()
+		
+		if self.xEvent.is_set():
+			print("Event is set true")
+			
+	def capture(self,window,logpath,keepalive):
+	    logfile = open(logpath,"r")
+	    keepalive = True
+	    loglines = self.follow(logfile,keepalive)
+	    
+	    self.lastLow = 10000
+	    self.lastHigh = 0
+	    LogReport = []
 
-def capture_log(window,logpath,keepalive):
-	# Capture log output
-    logfile = open(logpath,"r")
-    keepalive = True
-    loglines = follow(logfile,keepalive)
-    
-    lastLow = 10000
-    lastHigh = 0
-    #print ('For Start...')
-    LogReport = []
-    for line in loglines:
-    	# Need way to detect and break this when needed
-    	#  Looks like will need to stop the loop in follow()
-    	
-    	split = line.split(' ')
-    	#print(repr(split))
-    	if "was hit by" in line:
-    		split1 = line.split('DEBUG: ')
-    		pre = split1[0]
-    		post = split1[1]
-    		split2 = post.split(' ')
-    		
-    		hit = int(split2[8])
-    		
-    		if hit > lastHigh:
-    			lastHigh = hit
-    		
-    		if hit < lastLow:
-    			lastLow = hit
-    		
-    		LogReport.append("DMG: {0} [hi:{1}/lo:{2}] PEN: {3} ARM: {4} Visual: {5}".format(hit,lastHigh,lastLow,split2[10],split2[14],split2[6]))
-    		#print("DMG: {0} [hi:{1}/lo:{2}] PEN: {3} ARM: {4} Visual: {5}".format(hit,lastHigh,lastLow,split2[10],split2[14],split2[6]))
-    		window['LogOutput'].update(values=(LogReport))
+	    for line in loglines:
+	    	split = line.split(' ')
+	    	if "was hit by" in line:
+	    		split1 = line.split('DEBUG: ')
+	    		pre = split1[0]
+	    		post = split1[1]
+	    		split2 = post.split(' ')
+	    		
+	    		hit = int(split2[8])
+	    		
+	    		if hit > self.lastHigh:
+	    			self.lastHigh = hit
+	    		
+	    		if hit < self.lastLow:
+	    			self.lastLow = hit
+	    		
+	    		LogReport.append("DMG: {0} [Lo:{1}/Hi:{2}] PEN: {3} ARM: {4} Visual: {5}".format(hit,self.lastLow,self.lastHigh,split2[10],split2[14],split2[6]))
+	    		#print("DMG: {0} [hi:{1}/lo:{2}] PEN: {3} ARM: {4} Visual: {5}".format(hit,lastHigh,lastLow,split2[10],split2[14],split2[6]))
+	    		window['LogOutput'].update(values=(LogReport))
 
-def capture_start(window,logpath,keepalive):
-	# Start capturing
-	xEvent = threading.Event()
-	x = threading.Thread(target=capture_log, args=(window,logpath,True,))
-	x.start()
-	
-	return x, xEvent
-	
-def capture_stop(x,xEvent):
-	# Stop Capturing
-	print("Setting event")
-	xEvent.set()
-	
-	if xEvent.is_set():
-		print("Event is set true")
+	def ResetCounters(self):
+		print("Reset Counters")
+		self.lastHigh = 0
+		self.lastLow = 10000
+		
+	def ClearWindow(self,window):
+		print("Clear Window")
+		LogReport = []
+		window['LogOutput'].update(values=(LogReport))
